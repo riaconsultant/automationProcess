de6f459a26874ec9da3558d7c377c5278931790a
deepak <deepak@DESKTOP-TPMRULR>
2016-03-07 11:37:08 +0530

    Dashboard

--- APAutomation/WebContent/Component.js
+++ APAutomation/WebContent/Component.js
@@ -2,42 +2,71 @@ jQuery.sap.declare("incture.apautomation.Component");
 
 sap.ui.core.UIComponent.extend("incture.apautomation.Component",{
 	metadata:{
+		includes:[
+		          "./CSS/style.css",
+		          "./CSS/fonts.css"
+		          ],
 		routing:{
 			
 			config:{
 				viewType: "XML",
-				viewPath: "apautomation"
+				viewPath: "apautomation",
+					targetAggregation: "pages",
+					clearTarget: false
 			},
 			
 			routes:[
 			        {
-			        	pattern:"",
-			        	name:"MasterInvoice",
-			        	target:["master"],
-			        	subroutes:[
-										{
-											pattern:"InvoiceDetails/{Price}",
-											name:"DetailInvoice",
-											target:["detail"]
-										}
-			        	           ]
-			        }
-			        ],
-			        
-			targets:{
-				"master":{
-					viewName: "MasterInvoice",
-					type:"XML",
-					controlAggregation:"masterPages",
-					controlId:"idAppControl"
-				},
-				"detail":{
-					viewName:"DetailInvoice",
-					type:"XML",
-					controlAggregation:"detailPages",
-					controlId:"idAppControl"
-				}
-			}
+			        	pattern: "",
+						name: "LaunchScreen",
+						view: "LaunchScreen",
+						targetControl: "masterApp"
+//							,subroutes:[
+//										{
+//											pattern:"masterInvoice/{Price}",
+//											name:"masterInvoice",
+//											target:["master"]
+//										}
+//			        	           ]
+			        },{
+	                    pattern: "split",
+						name: "App",
+						view: "App",
+						targetControl: "masterApp",
+						subroutes: [
+	    				{
+	    					pattern: "masterInvoice/{contextPath}",
+	    					name: "masterInvoice",
+	    					view: "MasterInvoice",
+	    					targetAggregation: "masterPages",
+	    					targetControl: "idAppControl",
+	    					subroutes: [
+	    						{
+	    							pattern: "masterInvoice/{contextPath}/DetailInvoice/{Price}",
+	    							name: "DetailInvoice",
+	    							view: "DetailInvoice",
+	    							targetAggregation: "detailPages"
+	    						}
+	    					]
+	    				}]
+					}
+			        ]
+			,
+//			        
+//			targets:{
+//				"master":{
+//					viewName: "MasterInvoice",
+//					type:"XML",
+//					controlAggregation:"masterPages",
+//					controlId:"idAppControl"
+//				},
+//				"detail":{
+//					viewName:"DetailInvoice",
+//					type:"XML",
+//					controlAggregation:"detailPages",
+//					controlId:"idAppControl"
+//				}
+//			}
 		}
 	},
 	
@@ -48,20 +77,67 @@ sap.ui.core.UIComponent.extend("incture.apautomation.Component",{
 		sap.ui.core.UIComponent.prototype.init.apply(this,arguments);
 		
 		this.router= this.getRouter();
-		this.router.initialize();
+		
+		
+//		this.router.myNavBack = incture.apautomation.MyRouter.myNavBack;
+		// 4. initialize the router  
+		this.routeHandler = new sap.m.routing.RouteMatchedHandler(this.router);
+			this.router.initialize();
+//		router.initialize();
 	},
 	
 	createContent: function(){
 		var oView = sap.ui.view({
 			id:'app',
-			viewName:"apautomation.App",
+			viewName:"apautomation.masterApp",
 			type:"XML",
 			viewData: {component: this}
 		});
 		
 		var oModel = new sap.ui.model.json.JSONModel();
-		oModel.loadData('./json/InvoiceDetails.json',{async:false});
+		oModel.loadData('util/InvoiceDetails.json',{async:false});
 		oView.setModel(oModel,"defaultModel");
+		
+		var oModel = new sap.ui.model.json.JSONModel();
+		oModel.loadData("util/layout.json");
+		
+//		var oModel = new sap.ui.model.odata.ODataModel("https://ldcigm6.wdf.sap.corp:44333/sap/opu/odata/sap/TDMS_MANAGE_EXEC_SRV/");
+		this.setModel(oModel);
+		
+		var tileData = [{
+    		toolbar : "Customer",
+    		reportListDto : []
+			}];
+		var listModel = new sap.ui.model.json.JSONModel();
+			listModel.setData({reportListDto : [{
+		    				category: "Report",
+		    				reportDesc: "Report",
+		    				reportId: "Report",
+		    				reportName: "Report"
+		    		},{
+		    				category: "OTIFReport",
+		    				reportDesc: "OTIF Report",
+		    				reportId: "otifReport",
+		    				reportName: "OTIF Report"
+		    		},{
+						category: "POReport",
+						reportDesc: "PO Report",
+						reportId: "poReport",
+						reportName: "PO Report"
+				}]});
+		this.setModel(listModel, "listModel");
+		
+		var tileModel = new sap.ui.model.json.JSONModel();
+		tileModel.setData({tileData:tileData});
+		this.setModel(tileModel, "tileModel");
+		
+		var detailModel = new sap.ui.model.json.JSONModel();
+		this.setModel(detailModel, "detailModel");
+		
+		var oModel = new sap.ui.model.json.JSONModel();
+		oModel.loadData('util/InvoiceDetails.json',{async:false});
+		oView.setModel(oModel,"defaultModel");
+		
 		
 		return oView;
 	}
--- APAutomation/WebContent/apautomation/MasterInvoice.controller.js
+++ APAutomation/WebContent/apautomation/MasterInvoice.controller.js
@@ -5,17 +5,57 @@ sap.ui.controller("apautomation.MasterInvoice", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf apautomation.MasterInvoice
 */
-//	onInit: function() {
-//
-//	},
-	
+	onInit: function() {
+		this.router = sap.ui.core.UIComponent.getRouterFor(this);
+		var that = this;
+		this.router.getRoute("masterInvoice").attachPatternMatched(this._routePatternMatched, this);
+		
+		
+//		this.router.attachRoutePatternMatched(function(oEvent) {
+//			//context data from launcher screen
+//			var arguments = oEvent.getParameter("arguments").contextPath;
+//			if(arguments){
+//				var contextParams = JSON.parse(oEvent.getParameter("arguments").contextPath);
+//				that.getView().getModel("detailModel").setData(contextParams);
+//				var masterPage = that.getView().byId("masterPage");
+//				masterPage.addStyleClass(that.getView().getModel().getData().mainLayoutConf.navBgColor);
+//				
+//				//Data binding for master list
+//				var masterListModel = new sap.ui.model.json.JSONModel();
+//				masterListModel.setData(contextParams.contextData);
+//				var masterList = that.getView().byId("masterList");
+//				masterList.setModel(masterListModel);
+//				that.getView().byId("listItems").addStyleClass(that.getView().getModel().getData().mainLayoutConf.navTextColor);
+//				masterList.bindItems("/reportListDto", that.getView().byId("listItems"));
+//				masterList.setHeaderText(contextParams.contextData.toolbar);
+//				masterList.addStyleClass(that.getView().getModel().getData().mainLayoutConf.navTextColor);
+//				var contextData = that.getView().getModel("tileModel").getData().tileData[contextParams.tileModelData].reportListDto[contextParams.tilePath];
+//				for(var i = 0 ; i < masterList.getItems().length ; i++){
+//					if(contextData.reportName === masterList.getItems()[i].getTitle()){
+//						masterList.getItems()[i].addStyleClass(that.getView().getModel().getData().mainLayoutConf.navHighlightColor);
+//					}
+//				}
+//			}
+//			
+//			if (oEvent.getParameter("name") === "masterView") {
+//				//navigation to detail view
+//				that.router.navTo("detailView", {
+//					contextPath : arguments
+//				});
+//			}
+//		});
+	},
+	_routePatternMatched: function(oEvent) {
+	 context = oEvent.getParameter("arguments").contextPath;
+
+	},
 	handleItemPress: function(oEvent){
 		this.router= sap.ui.core.UIComponent.getRouterFor(this);
 		var item= oEvent.getSource();
 		var price= item.getBindingContextPath("defaultModel").split("/")[2];
 		
-		
-		this.router.navTo("DetailInvoice",{Price:price});
+//		this._router.navTo("product", {id: sCategoryId, productId: sProductId}, !Device.system.phone);
+		this.router.navTo("DetailInvoice",{contextPath:context, Price:price},false);
 	}
 
 /**
